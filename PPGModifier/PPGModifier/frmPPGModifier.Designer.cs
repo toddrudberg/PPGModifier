ecd3b97a8115a025ea7730d689918c426dd8077a
toddrudberg <todd.rudberg@icloud.com>
2026-01-21 11:40:33 -0800

    working on v1.12 conversion

--- PPGModifier/PPGModifier/frmPPGModifier.Designer.cs
+++ PPGModifier/PPGModifier/frmPPGModifier.Designer.cs
@@ -32,6 +32,7 @@
       btnDecoupleROTX = new Button();
       btnMoveCompaction = new Button();
       btnMoveCutPrepare = new Button();
+      button1 = new Button();
       SuspendLayout();
       // 
       // btnDoIt
@@ -75,11 +76,23 @@
       btnMoveCutPrepare.UseVisualStyleBackColor = true;
       btnMoveCutPrepare.Click += btnMoveCutPrepare_Click;
       // 
+      // button1
+      // 
+      button1.Location = new Point(158, 156);
+      button1.Margin = new Padding(1);
+      button1.Name = "button1";
+      button1.Size = new Size(331, 41);
+      button1.TabIndex = 4;
+      button1.Text = "Do The Thing 1.12";
+      button1.UseVisualStyleBackColor = true;
+      button1.Click += button1_Click;
+      // 
       // frmPPGModifier
       // 
       AutoScaleDimensions = new SizeF(7F, 15F);
       AutoScaleMode = AutoScaleMode.Font;
       ClientSize = new Size(705, 546);
+      Controls.Add(button1);
       Controls.Add(btnMoveCutPrepare);
       Controls.Add(btnMoveCompaction);
       Controls.Add(btnDecoupleROTX);
@@ -97,5 +110,6 @@
     private Button btnDecoupleROTX;
     private Button btnMoveCompaction;
     private Button btnMoveCutPrepare;
+    private Button button1;
   }
 }
